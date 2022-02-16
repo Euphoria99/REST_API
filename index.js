f58e9b499e2f765ef54d3a0d31b24884aca01667
Pavan Bhat <363pavanbhat@gmail.com>
2022-02-16 21:28:52 +0530

    Added gitignore file

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
+// import 'dotenv/config' 
 import express  from "express";
 import bodyParser from 'body-parser';
 import res from "express/lib/response.js";
 
+
+
+
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
 import usersRoutes from './routes/users.js';
+
+// dotenv.config( { path : '.env'} )
+// require('dotenv').config()
+const connectDB = require('./server/database/connection.cjs');
+
+// import connectDB from '../server/database/connection.js';
 
 const app = express();
 const PORT = 5000;
 
+connectDB;
 
 app.use(bodyParser.json());
 
